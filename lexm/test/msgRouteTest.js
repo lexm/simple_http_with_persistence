4af12cc027182ada5a694e6118f7047864522d32
Lex Myers <lex.myers@gmail.com>
2016-03-10 12:45:11 -0800

    attempted first test

--- lexm/test/msgRouteTest.js
+++ lexm/test/msgRouteTest.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var chai = require('chai');
 var chaiHTTP = require('chai-http');
 chai.use(chaiHTTP);
@@ -7,10 +8,26 @@ var expect = chai.expect;
 require(__dirname + '/../routes/seriesRoutes');
 
 describe('Testing series POST', () => {
+  before((done) => {
+    var numFiles;
+    fs.readdir('../data', (err, files) => {
+      numFiles = files.length;
+      done();
+    });
+  });
   it('should respond to POST request by creating a series', (done) => {
     request('localhost:3000')
       .post('/series')
       .set('Content-Type', 'application/json')
+      .send('{"name": "The Wire"}')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(400);
+        fs.readdir('../data', (err, files) => {
+          expect(files.length).to.eql(numFiles + 1);
+        });
+        done();
+      });
 
   });
 });
